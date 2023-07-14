397ec9dd58ea10f5c1c885e54c952e4ebca4e490
JyothikSandeep <101253647+JyothikSandeep@users.noreply.github.com>
2023-07-14 15:45:13 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const val = document.getElementById("display");
+const startbtn = document.getElementById("startbtn");
+const stopbtn = document.getElementById("stopbtn");
+const resetbtn = document.getElementById("resetbtn");
+console.log(document.getElementById("startbtn").value);
+let msec = 0;
+let secs = 0;
+let mins = 0;
+
+let timerId = null;
+
+startbtn.addEventListener("click", function () {
+  if (timerId !== null) {
+    clearInterval(timerId);
+  }
+  timerId = setInterval(startTimer, 10);
+  console.log(timerId);
+});
+stopbtn.addEventListener("click", function () {
+  clearInterval(timerId);
+});
+resetbtn.addEventListener("click", function () {
+  clearInterval(timerId);
+  console.log(timerId);
+
+  val.value = `00 : 00 : 00`;
+  msec = 0;
+  secs = 0;
+  mins = 0;
+});
+
+function startTimer() {
+  msec++;
+  if (msec == 100) {
+    msec = 0;
+    secs++;
+    if (secs == 60) {
+      secs = 0;
+      mins++;
+    }
+  }
+  let msecString = msec < 10 ? `0${msec}` : msec;
+  let secsString = secs < 10 ? `0${secs}` : secs;
+  let minsString = mins < 10 ? `0${mins}` : mins;
+  val.value = `${minsString} : ${secsString} : ${msecString}`;
+}
